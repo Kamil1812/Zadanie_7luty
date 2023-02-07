5b78d5e33e48c95f76f32256ad425a5e87acd531
Filippo0420 <fpawelec0@gmail.com>
2023-02-07 08:59:35 +0100

    ProgressBar value & implementation of imageviews

--- app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
+++ app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pablo.zadanie_7luty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.SeekBar
 
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
         var progressBar_horizontal = findViewById<ProgressBar>(R.id.progressBar_horizontal)
         var progressBar_vertical = findViewById<ProgressBar>(R.id.progressBar_vertical)
 
+        var picture_red = findViewById<ImageView>(R.id.imageView_red)
+        var picture_green = findViewById<ImageView>(R.id.imageView_green)
+        var picture_blue = findViewById<ImageView>(R.id.imageView_blue)
+
         findViewById<Button>(R.id.btn_reset).setOnClickListener {
             seekBar_horizontal_red.setProgress(50)
             seekBar_horizontal_green.setProgress(50)
@@ -30,6 +35,12 @@ class MainActivity : AppCompatActivity() {
             seekBar_vertical_red.setProgress(50)
             seekBar_vertical_green.setProgress(50)
             seekBar_vertical_blue.setProgress(50)
+
+            var horizontal_progress = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
+            progressBar_horizontal.setProgress(horizontal_progress)
+
+            var vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
+            progressBar_vertical.setProgress(vertical_progress)
         }
     }
 }
