541189769ba7eaf16f3560a384f110c534cecdc3
Filippo0420 <fpawelec0@gmail.com>
2023-02-07 07:23:01 +0100

    Logowanie GitHub

---
+++ app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pablo.zadanie_7luty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
