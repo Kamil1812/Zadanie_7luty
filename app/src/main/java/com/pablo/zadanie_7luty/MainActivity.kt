9badd7ff513d2587b9526fce0b41e36db9543ff4
Filippo0420 <fpawelec0@gmail.com>
2023-02-07 09:24:19 +0100

    ProgressBar action (changing beacuse of changing SeekBar value)

--- app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
+++ app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
@@ -23,9 +23,17 @@ class MainActivity : AppCompatActivity() {
         var progressBar_horizontal = findViewById<ProgressBar>(R.id.progressBar_horizontal)
         var progressBar_vertical = findViewById<ProgressBar>(R.id.progressBar_vertical)
 
+        var horizontal_progress = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
+        progressBar_horizontal.setProgress(horizontal_progress)
+
+        var vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
+        progressBar_vertical.setProgress(vertical_progress)
+
         var picture_red = findViewById<ImageView>(R.id.imageView_red)
         var picture_green = findViewById<ImageView>(R.id.imageView_green)
         var picture_blue = findViewById<ImageView>(R.id.imageView_blue)
+
+
 
         findViewById<Button>(R.id.btn_reset).setOnClickListener {
             seekBar_horizontal_red.setProgress(50)
@@ -35,12 +43,108 @@ class MainActivity : AppCompatActivity() {
             seekBar_vertical_red.setProgress(50)
             seekBar_vertical_green.setProgress(50)
             seekBar_vertical_blue.setProgress(50)
-
-            var horizontal_progress = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
-            progressBar_horizontal.setProgress(horizontal_progress)
-
-            var vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
-            progressBar_vertical.setProgress(vertical_progress)
         }
+
+        // HORIZONTAL RED
+        seekBar_horizontal_red?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
+                progressBar_horizontal.setProgress(horizontal_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+        // HORIZONTAL GREEN
+        seekBar_horizontal_green?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
+                progressBar_horizontal.setProgress(horizontal_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+        // HORIZONTAL BLUE
+        seekBar_horizontal_blue?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
+                progressBar_horizontal.setProgress(horizontal_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+        // VERTICAL RED
+        seekBar_vertical_red?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
+                progressBar_vertical.setProgress(vertical_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+        // VERTICAL GREEN
+        seekBar_vertical_green?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
+                progressBar_vertical.setProgress(vertical_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+        // VERTICAL BLUE
+        seekBar_vertical_blue?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
+                progressBar_vertical.setProgress(vertical_progress)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
     }
 }
