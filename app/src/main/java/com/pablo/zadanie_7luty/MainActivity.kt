131b2ce8b028388532eb54643b31522493485a44
Filippo0420 <fpawelec0@gmail.com>
2023-02-14 08:29:05 +0100

    Koniec - SeekBar & ProgressBar

--- app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
+++ app/src/main/java/com/pablo/zadanie_7luty/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.pablo.zadanie_7luty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +35,13 @@ class MainActivity : AppCompatActivity() {
         var picture_green = findViewById<ImageView>(R.id.imageView_green)
         var picture_blue = findViewById<ImageView>(R.id.imageView_blue)
 
+        var red_vertical_value = seekBar_vertical_red.progress - 50
+        var green_vertical_value = seekBar_vertical_green.progress - 50
+        var blue_vertical_value = seekBar_vertical_blue.progress - 50
+
+        var red_horizontal_value = seekBar_horizontal_red.progress - 50
+        var green_horizontal_value = seekBar_horizontal_green.progress - 50
+
         findViewById<Button>(R.id.btn_reset).setOnClickListener {
             seekBar_horizontal_red.setProgress(50)
             seekBar_horizontal_green.setProgress(50)
@@ -43,12 +52,29 @@ class MainActivity : AppCompatActivity() {
             seekBar_vertical_blue.setProgress(50)
         }
 
+        val default = picture_blue.layoutParams.width
+
         // HORIZONTAL RED
         seekBar_horizontal_red?.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
                 progressBar_horizontal.setProgress(horizontal_progress)
+
+
+                var red_horizontal_value = seekBar_horizontal_red.progress / 100f
+                var layout = picture_red.layoutParams
+
+                if(red_horizontal_value == 0f)
+                {
+                    layout.width = 1
+                }
+                else
+                {
+                    var value = default * red_horizontal_value * 2
+                    layout.width = value.toInt()
+                }
+                picture_red.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -66,6 +92,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
                 progressBar_horizontal.setProgress(horizontal_progress)
+
+                var green_horizontal_value = seekBar_horizontal_green.progress / 100f
+                var layout = picture_green.layoutParams
+
+                if(green_horizontal_value == 0f)
+                {
+                    layout.width = 1
+                }
+                else
+                {
+                    var value = default * green_horizontal_value * 2
+                    layout.width = value.toInt()
+                }
+                picture_green.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -83,6 +123,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 horizontal_progress  = (seekBar_horizontal_red.progress + seekBar_horizontal_green.progress + seekBar_horizontal_blue.progress) / 3
                 progressBar_horizontal.setProgress(horizontal_progress)
+
+                var blue_horizontal_value = seekBar_horizontal_blue.progress / 100f
+                var layout = picture_blue.layoutParams
+
+                if(blue_horizontal_value == 0f)
+                {
+                    layout.width = 1
+                }
+                else
+                {
+                    var value = default * blue_horizontal_value * 2
+                    layout.width = value.toInt()
+                }
+                picture_blue.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -100,6 +154,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
                 progressBar_vertical.setProgress(vertical_progress)
+
+                var red_vertical_value = seekBar_vertical_red.progress / 100f
+                var layout = picture_red.layoutParams
+
+                if(red_vertical_value == 0f)
+                {
+                    layout.height = 1
+                }
+                else
+                {
+                    var value = default * red_vertical_value * 2
+                    layout.height = value.toInt()
+                }
+                picture_red.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -117,6 +185,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
                 progressBar_vertical.setProgress(vertical_progress)
+
+                var green_vertical_value = seekBar_vertical_green.progress / 100f
+                var layout = picture_green.layoutParams
+
+                if(green_vertical_value == 0f)
+                {
+                    layout.height = 1
+                }
+                else
+                {
+                    var value = default * green_vertical_value * 2
+                    layout.height = value.toInt()
+                }
+                picture_green.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -134,6 +216,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
                 vertical_progress = (seekBar_vertical_red.progress + seekBar_vertical_green.progress + seekBar_vertical_blue.progress) / 3
                 progressBar_vertical.setProgress(vertical_progress)
+
+                var blue_vertical_value = seekBar_vertical_blue.progress / 100f
+                var layout = picture_blue.layoutParams
+
+                if(blue_vertical_value == 0f)
+                {
+                    layout.height = 1
+                }
+                else
+                {
+                    var value = default * blue_vertical_value * 2
+                    layout.height = value.toInt()
+                }
+                picture_blue.layoutParams = layout
             }
 
             override fun onStartTrackingTouch(seek: SeekBar) {
@@ -143,6 +239,6 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seek: SeekBar) {
 
             }
-        })
+            })
     }
 }
